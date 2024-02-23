59a32dc6a8122ae2b53482884b92a2796e23961e
Ryan Hammer <ryanhammer1@protonmail.com>
2024-02-23 16:36:23 -0500

    add search bar without functionality

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,12 +1,14 @@
 import BasketballRefLogo from './basketball-ref-logo';
-import { SportsReferenceLinksBar } from './sports-reference-links-bar';
+import { SearchBar } from './search-bar';
+import { TopLinksBar } from './top-links-bar';
 
-export function Header({ className }: { className?: string }) {
+export function Header() {
   return (
-    <header className='lg:max-w-[960[px] py-2 flex flex-col'>
-      <SportsReferenceLinksBar />
-      <div>
+    <header className='lg:max-w-[960[px]flex flex-col'>
+      <TopLinksBar />
+      <div className='my-2 flex justify-between'>
         <BasketballRefLogo className='w-64' />
+        <SearchBar />
       </div>
     </header>
   );
