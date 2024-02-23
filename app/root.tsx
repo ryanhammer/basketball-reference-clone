25e1e0e106c1f5272088226e022f48ec1a01a1db
Ryan Hammer <ryanhammer1@protonmail.com>
2024-02-23 15:17:54 -0500

    import stylesheet and size root container

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,32 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { cssBundleHref } from '@remix-run/css-bundle';
+import type { LinksFunction } from '@remix-run/node';
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import tailwindStylesheet from './index.css?url';
+import { Header } from './components/header';
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: 'stylesheet', href: tailwindStylesheet },
+  ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
 ];
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
         <Links />
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
+        <div className='flex justify-center bg-light-gray min-h-screen'>
+          <div className='bg-white xl:w-[1476px] shadow-md mx-auto'>
+            <Header className='fixed top-0 w-full' />
+            <Outlet />
+            <ScrollRestoration />
+            <Scripts />
+          </div>
+        </div>
       </body>
     </html>
   );
