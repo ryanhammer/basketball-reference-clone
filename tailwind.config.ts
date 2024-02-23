fa60c221503735edffd629f63308418a85c7c658
Ryan Hammer <ryanhammer1@protonmail.com>
2024-02-23 15:17:23 -0500

    add css file and update tailwind colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        'honey-brown': '#724300',
+        'schrute-yellow': '#D47D00',
+        charcoal: '#454341',
+        'off-white': '#EFEEED',
+        'light-gray': '#C9CBCD',
+        'slate-gray': '#45433F',
+        'silver-gray': '#757678',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
