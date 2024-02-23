8a09099cb177a868e7bc7b7ee2712349262bf74a
Ryan Hammer <ryanhammer1@protonmail.com>
2024-02-23 16:36:05 -0500

    rename component

--- app/components/header/top-links-bar.tsx
+++ app/components/header/top-links-bar.tsx
@@ -12,7 +12,7 @@ interface MultipleLinksData {
 
 type LinksData = SingleLinkData | MultipleLinksData;
 
-export function SportsReferenceLinksBar(): JSX.Element {
+export function TopLinksBar(): JSX.Element {
   const linksData: LinksData[] = [
     {
       linkText: 'Baseball',
@@ -49,15 +49,41 @@ export function SportsReferenceLinksBar(): JSX.Element {
   ];
 
   return (
-    <div className='flex justify-between bg-slate-gray py-1'>
-      <div>
-        <div className='border-r-2 border-light-gray'>
+    <div className='flex justify-between bg-slate-gray pt-1 pb-1.5 h-6'>
+      <div className='flex justify-start'>
+        <div className='border-r-[1px] border-light-gray border-opacity-50 pr-2'>
           <Link to='https://www.sports-reference.com/' className='flex text-light-gray text-xs'>
-            <img src='sports-ref-logo.png' alt='Sports Reference' className='w-6 h-5' />
+            <img src='sports-ref-logo.png' alt='Sports Reference' className='w-6 h-4' />
             {' Sports Reference ®'}
           </Link>
         </div>
+        {linksData.map((linkData, index) => (
+          <LinkSection linkData={linkData} key={index} isLastLink={index === linksData.length - 1} />
+        ))}
       </div>
+    </div>
+  );
+}
+
+function LinkSection({ linkData, isLastLink }: { linkData: LinksData; isLastLink: boolean }): JSX.Element {
+  const borderStyles = isLastLink ? '' : 'border-r-[1px] border-light-gray border-opacity-50';
+  if ('linkText' in linkData) {
+    return (
+      <div className={`flex ${borderStyles} px-2`}>
+        <Link to={linkData.href} className='text-light-gray text-xs'>
+          {linkData.linkText}
+        </Link>
+      </div>
+    );
+  }
+
+  return (
+    <div className={`flex ${borderStyles} px-2`}>
+      {linkData.linksText.map((linkText, index) => (
+        <Link to={linkData.hrefs[index]} className='text-light-gray text-xs' key={linkText}>
+          {linkText}
+        </Link>
+      ))}
     </div>
   );
 }
