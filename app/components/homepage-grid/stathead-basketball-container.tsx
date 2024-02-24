da6f3ebdd8a3a0cec4c3cfffcbab3c8faac7bea7
Ryan Hammer <ryanhammer1@protonmail.com>
2024-02-23 19:20:55 -0500

    add placeholders for grid

---
+++ app/components/homepage-grid/stathead-basketball-container.tsx
@@ -0,0 +1,8 @@
+export function StatheadBasketballContainer({ className }: { className?: string }): JSX.Element {
+  return (
+    <div className={`${className} border-2 border-gray-500`}>
+      <h2>Powered by Stathead</h2>
+      <p>Some text about Stathead Basketball</p>
+    </div>
+  );
+}
