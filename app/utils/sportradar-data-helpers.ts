2697c8105cb5930303912787a1f65d46cd82d113
Ryan Hammer <ryanhammer1@protonmail.com>
2024-02-25 11:49:40 -0500

    more game summary data prep

--- app/utils/sportradar-data-helpers.ts
+++ app/utils/sportradar-data-helpers.ts
@@ -32,6 +32,9 @@ export async function prepareGameSummaryToGame(
     awayTeamOTPoints = away.scoring.slice(4).reduce((acc, scoring) => acc + scoring.points, 0);
   }
 
+  const [homeTeamGameInput, awayTeamGameInput] = await Promise.all([prepareGameSummaryToTeamGames(gameSummary, homeTeam.id),
+    prepareGameSummaryToTeamGames(gameSummary, awayTeam.id)]);
+
   return {
     externalId: id,
     homeTeamId: homeTeam.id,
@@ -55,6 +58,13 @@ export async function prepareGameSummaryToGame(
     awayTeamQ4Points: away.scoring[3].points,
     awayTeamOTPoints,
     notes: gameSummary.inseason_tournament ? 'Inseason Tournament' : null,
+    teamGames: {
+      createMany: {
+        data: [
+          homeTeamGameInput, awayTeamGameInput
+        ]
+      }
+    }
   };
 }
 
@@ -64,3 +74,111 @@ function createVenueData({ id, ...rest }: SportradarVenue) {
     ...rest,
   };
 }
+
+export async function prepareGameSummaryToTeamGames(gameSummary: GameSummary, teamSeasonId: string): Promise<Prisma.TeamGameCreateManySeasonGameInput> {
+  const secondsPlayed = convertGameMinutesToSeconds(gameSummary.duration) * 5;
+  
+  return {
+    teamSeasonId,
+    points: gameSummary.home.points,
+    oppPoints: gameSummary.away.points,
+    secondsPlayed,
+    fieldGoals: gameSummary.home.statistics.field_goals_made,
+    fieldGoalAttempts: gameSummary.home.statistics.field_goals_att,
+    threePointers: gameSummary.home.statistics.three_points_made,
+    threePointAttempts: gameSummary.home.statistics.three_points_att,
+    twoPointers: gameSummary.home.statistics.two_points_made,
+    twoPointAttempts: gameSummary.home.statistics.two_points_att,
+    freeThrows: gameSummary.home.statistics.free_throws_made,
+    freeThrowAttempts: gameSummary.home.statistics.free_throws_att,
+    assists: gameSummary.home.statistics.assists,
+    rebounds: gameSummary.home.statistics.team_rebounds,
+    offRebounds: gameSummary.home.statistics.team_offensive_rebounds,
+    defRebounds: gameSummary.home.statistics.team_defensive_rebounds,
+    steals: gameSummary.home.statistics.steals,
+    blocks: gameSummary.home.statistics.blocks,
+    turnovers: gameSummary.home.statistics.team_turnovers,
+    personalFouls: gameSummary.home.statistics.personal_fouls,
+    possessions: gameSummary.home.statistics.possessions, // TODO: calculate possessions
+    oppPossessions: gameSummary.away.statistics.possessions,
+    pace: (gameSummary.home.statistics.possessions + gameSummary.away.statistics.possessions)/(2 * (secondsPlayed/(60 * 5))/48),
+    offRating: gameSummary.home.statistics.offensive_rating, // TODO: calculate offensive rating
+    defRating: gameSummary.home.statistics.defensive_rating,
+  }
+}
+
+export async function prepareGameSummaryToPlayerGames({gameSummary, teamPossessionCount}: {gameSummary: GameSummary; teamPossessionCount: number}): Promise<Prisma.PlayerGameCreateArgs['data']> {
+  return {
+    playerId             String   
+    gameId               String   
+    playerSeasonId       String   
+    points               Int
+    secondsPlayed        Int      
+    fieldGoals           Int      
+    fieldGoalAttempts    Int      
+    threePointers        Int      
+    threePointAttempts   Int      
+    twoPointers          Int      
+    twoPointAttempts     Int      
+    freeThrows           Int      
+    freeThrowAttempts    Int      
+    assists              Int
+    rebounds             Int
+    offRebounds          Int      
+    defRebounds          Int      
+    steals               Int
+    blocks               Int
+    turnovers            Int
+    personalFouls        Int      
+    plusMinus            Int      
+    offensivePossessions Decimal  @default(0) @map("offensive_possessions")
+    defensivePossessions Decimal  @default(0) @map("defensive_possessions")
+    createdAt            DateTime @default(now()) @map("created_at")
+    updatedAt            DateTime @updatedAt @map("updated_at")
+}
+
+id                   String   @id @default(cuid())
+  playerId             String   @map("player_id")
+  teamSeasonId         String   @map("team_season_id")
+  seasonId             String   @map("season_id")
+  gamesPlayed          Int      @default(0) @map("games_played")
+  gamesStarted         Int      @default(0) @map("games_started")
+  isTwoWayContract     Boolean  @map("is_two_way_contract")
+  jerseyNumbers        Int[]    @map("jersey_number")
+  points               Int      @default(0)
+  secondsPlayed        Int      @default(0) @map("seconds_played")
+  fieldGoals           Int      @default(0) @map("field_goals")
+  fieldGoalAttempts    Int      @default(0) @map("field_goal_attempts")
+  threePointers        Int      @default(0) @map("three_pointers")
+  threePointAttempts   Int      @default(0) @map("three_point_attempts")
+  twoPointers          Int      @default(0) @map("two_pointers")
+  twoPointAttempts     Int      @default(0) @map("two_point_attempts")
+  freeThrows           Int      @default(0) @map("free_throws")
+  freeThrowAttempts    Int      @default(0) @map("free_throw_attempts")
+  assists              Int      @default(0)
+  rebounds             Int      @default(0)
+  offRebounds          Int      @default(0) @map("off_rebounds")
+  defRebounds          Int      @default(0) @map("def_rebounds")
+  steals               Int      @default(0)
+  blocks               Int      @default(0)
+  turnovers            Int      @default(0)
+  personalFouls        Int      @default(0) @map("personal_fouls")
+  plusMinus            Int      @default(0) @map("plus_minus")
+  offensivePossessions Decimal  @default(0) @map("offensive_possessions")
+  defensivePossessions Decimal  @default(0) @map("defensive_possessions")
+  createdAt            DateTime @default(now()) @map("created_at")
+  updatedAt            DateTime @updatedAt @map("updated_at")
+
+  player      Player       @relation(fields: [playerId], references: [id])
+  teamSeason  TeamSeason   @relation(fields: [teamSeasonId], references: [id])
+  season      Season       @relation(fields: [seasonId], references: [id])
+  playerGames PlayerGame[]
+
+  @@map("player_season")
+}
+
+function convertGameMinutesToSeconds(minutes: string): number {
+  return Number(minutes.split(':')[0]) * 60 + Number(minutes.split(':')[1]);
+}
+
+const playerGameCreate = 
