93b9d97749a60d22129b6055f6e1a93774edc249
Ryan Hammer <ryanhammer1@protonmail.com>
2024-02-24 21:13:12 -0500

    more access functions

--- db/access/season.ts
+++ db/access/season.ts
@@ -1,7 +1,11 @@
-import { platformDB } from '../prisma';
 import { Season, SeasonType } from '@prisma/client';
+import { platformDB } from '../prisma';
 
-export async function getSeasonByLeagueYearAndType(leagueId: string, year: number, type: SeasonType): Promise<Season> {
+export async function getSeasonByLeagueYearAndType({
+  leagueId,
+  year,
+  type,
+}: { leagueId: string; year: number; type: SeasonType }): Promise<Season> {
   return platformDB.season.findFirstOrThrow({
     where: {
       leagueId,
--- db/access/team.ts
+++ db/access/team.ts
@@ -1,5 +1,5 @@
-import { platformDB } from '../prisma';
 import { Team } from '@prisma/client';
+import { platformDB } from '../prisma';
 
 export async function getTeamByExternalId(externalId: string): Promise<Team> {
   return platformDB.team.findFirstOrThrow({
