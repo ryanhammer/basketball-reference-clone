c6651adf23a6fe81a393bbbc597247b3f5f92e60
Ryan Hammer <ryanhammer1@protonmail.com>
2024-02-25 19:48:43 -0500

    add helpers for preparing player game and coach game data

--- app/utils/sportradar-data-helpers.ts
+++ app/utils/sportradar-data-helpers.ts
@@ -1,9 +1,25 @@
-import { Prisma } from '@prisma/client';
+import { PlayerSeason, Prisma } from '@prisma/client';
 import { utcToZonedTime, format } from 'date-fns-tz';
-import { GameSummary, Venue as SportradarVenue } from '../types/sportrader';
+import {
+  GameSummary,
+  TeamGameSummary,
+  PlayerGameSummary,
+  CoachData,
+  Venue as SportradarVenue,
+} from '../types/sportrader';
 import { getSeasonByLeagueYearAndType } from '../../db/access/season';
 import { getTeamByExternalId } from '../../db/access/team';
 import { createVenue, getVenueByExternalId } from '../../db/access/venue';
+import { getPlayerByExternalId } from '../../db/access/player';
+import {
+  getPlayerSeasonByPlayerIdAndTeamSeasonId,
+  createPlayerSeason,
+  updatePlayerSeasonJerseyNumbers,
+  updatePlayerSeason,
+} from '../../db/access/player-season';
+import { createPlayerGame } from '../../db/access/player-game';
+import { createCoach, getCoachByExternalId } from '../../db/access/coach';
+import { getTeamGameByTeamSeasonIdAndGameId } from '../../db/access/team-game';
 
 export async function prepareGameSummaryToGame(
   gameSummary: GameSummary,
@@ -32,8 +48,20 @@ export async function prepareGameSummaryToGame(
     awayTeamOTPoints = away.scoring.slice(4).reduce((acc, scoring) => acc + scoring.points, 0);
   }
 
-  const [homeTeamGameInput, awayTeamGameInput] = await Promise.all([prepareGameSummaryToTeamGames(gameSummary, homeTeam.id),
-    prepareGameSummaryToTeamGames(gameSummary, awayTeam.id)]);
+  const [homeTeamGameInput, awayTeamGameInput] = await Promise.all([
+    prepareGameSummaryToTeamGames({
+      gameSummaryDuration: gameSummary.duration,
+      teamGameSummary: home,
+      opponentGameSummary: away,
+      teamSeasonId: homeTeam.id,
+    }),
+    prepareGameSummaryToTeamGames({
+      gameSummaryDuration: gameSummary.duration,
+      teamGameSummary: away,
+      opponentGameSummary: home,
+      teamSeasonId: awayTeam.id,
+    }),
+  ]);
 
   return {
     externalId: id,
@@ -60,11 +88,9 @@ export async function prepareGameSummaryToGame(
     notes: gameSummary.inseason_tournament ? 'Inseason Tournament' : null,
     teamGames: {
       createMany: {
-        data: [
-          homeTeamGameInput, awayTeamGameInput
-        ]
-      }
-    }
+        data: [homeTeamGameInput, awayTeamGameInput],
+      },
+    },
   };
 }
 
@@ -75,110 +101,185 @@ function createVenueData({ id, ...rest }: SportradarVenue) {
   };
 }
 
-export async function prepareGameSummaryToTeamGames(gameSummary: GameSummary, teamSeasonId: string): Promise<Prisma.TeamGameCreateManySeasonGameInput> {
-  const secondsPlayed = convertGameMinutesToSeconds(gameSummary.duration) * 5;
-  
+export async function prepareGameSummaryToTeamGames({
+  teamSeasonId,
+  teamGameSummary,
+  opponentGameSummary,
+  gameSummaryDuration,
+}: {
+  teamSeasonId: string;
+  teamGameSummary: TeamGameSummary;
+  opponentGameSummary: TeamGameSummary;
+  gameSummaryDuration: string;
+}): Promise<Prisma.TeamGameCreateManySeasonGameInput> {
+  const secondsPlayed = convertGameMinutesToSeconds(gameSummaryDuration) * 5;
+
   return {
     teamSeasonId,
-    points: gameSummary.home.points,
-    oppPoints: gameSummary.away.points,
+    points: teamGameSummary.points,
+    oppPoints: opponentGameSummary.points,
     secondsPlayed,
-    fieldGoals: gameSummary.home.statistics.field_goals_made,
-    fieldGoalAttempts: gameSummary.home.statistics.field_goals_att,
-    threePointers: gameSummary.home.statistics.three_points_made,
-    threePointAttempts: gameSummary.home.statistics.three_points_att,
-    twoPointers: gameSummary.home.statistics.two_points_made,
-    twoPointAttempts: gameSummary.home.statistics.two_points_att,
-    freeThrows: gameSummary.home.statistics.free_throws_made,
-    freeThrowAttempts: gameSummary.home.statistics.free_throws_att,
-    assists: gameSummary.home.statistics.assists,
-    rebounds: gameSummary.home.statistics.team_rebounds,
-    offRebounds: gameSummary.home.statistics.team_offensive_rebounds,
-    defRebounds: gameSummary.home.statistics.team_defensive_rebounds,
-    steals: gameSummary.home.statistics.steals,
-    blocks: gameSummary.home.statistics.blocks,
-    turnovers: gameSummary.home.statistics.team_turnovers,
-    personalFouls: gameSummary.home.statistics.personal_fouls,
-    possessions: gameSummary.home.statistics.possessions, // TODO: calculate possessions
-    oppPossessions: gameSummary.away.statistics.possessions,
-    pace: (gameSummary.home.statistics.possessions + gameSummary.away.statistics.possessions)/(2 * (secondsPlayed/(60 * 5))/48),
-    offRating: gameSummary.home.statistics.offensive_rating, // TODO: calculate offensive rating
-    defRating: gameSummary.home.statistics.defensive_rating,
+    fieldGoals: teamGameSummary.statistics.field_goals_made,
+    fieldGoalAttempts: teamGameSummary.statistics.field_goals_att,
+    threePointers: teamGameSummary.statistics.three_points_made,
+    threePointAttempts: teamGameSummary.statistics.three_points_att,
+    twoPointers: teamGameSummary.statistics.two_points_made,
+    twoPointAttempts: teamGameSummary.statistics.two_points_att,
+    freeThrows: teamGameSummary.statistics.free_throws_made,
+    freeThrowAttempts: teamGameSummary.statistics.free_throws_att,
+    assists: teamGameSummary.statistics.assists,
+    rebounds: teamGameSummary.statistics.team_rebounds,
+    offRebounds: teamGameSummary.statistics.team_offensive_rebounds,
+    defRebounds: teamGameSummary.statistics.team_defensive_rebounds,
+    steals: teamGameSummary.statistics.steals,
+    blocks: teamGameSummary.statistics.blocks,
+    turnovers: teamGameSummary.statistics.team_turnovers,
+    personalFouls: teamGameSummary.statistics.personal_fouls,
+    possessions: teamGameSummary.statistics.possessions, // TODO: calculate possessions
+    oppPossessions: opponentGameSummary.statistics.possessions,
+    pace:
+      (teamGameSummary.statistics.possessions + opponentGameSummary.statistics.possessions) /
+      ((2 * (secondsPlayed / (60 * 5))) / 48),
+    offRating: teamGameSummary.statistics.offensive_rating, // TODO: calculate offensive rating
+    defRating: opponentGameSummary.statistics.defensive_rating,
+  };
+}
+
+export async function preparePlayerGameSummaryToPlayerGameAndPlayerSeason({
+  playerGameSummary,
+  gameId,
+  seasonId,
+  teamSeasonId,
+  teamPossessionCount,
+  opponentPossessionCount,
+  teamGameDuration,
+}: {
+  playerGameSummary: PlayerGameSummary;
+  gameId: string;
+  seasonId: string;
+  teamSeasonId: string;
+  teamPossessionCount: number;
+  opponentPossessionCount: number;
+  teamGameDuration: string;
+}): Promise<{ playerGameCreateData: Prisma.PlayerGameCreateArgs['data']; playerSeason: PlayerSeason }> {
+  const player = await getPlayerByExternalId(playerGameSummary.id);
+  let playerSeason = await getPlayerSeasonByPlayerIdAndTeamSeasonId({ playerId: player.id, teamSeasonId });
+
+  if (playerSeason && !playerSeason.jerseyNumbers.includes(Number(playerGameSummary.jersey_number))) {
+    playerSeason = await updatePlayerSeasonJerseyNumbers(playerSeason.id, [
+      ...playerSeason.jerseyNumbers,
+      Number(playerGameSummary.jersey_number),
+    ]);
   }
+
+  if (!playerSeason) {
+    playerSeason = await createPlayerSeason({
+      playerId: player.id,
+      teamSeasonId,
+      seasonId,
+      isTwoWayContract: false,
+      jerseyNumbers: [Number(playerGameSummary.jersey_number)],
+    });
+  }
+
+  const teamSecondsPlayed = convertGameMinutesToSeconds(teamGameDuration);
+  const secondsPlayed = convertGameMinutesToSeconds(playerGameSummary.statistics.minutes);
+
+  const playerGameCreateData = {
+    playerId: player.id,
+    gameId,
+    playerSeasonId: playerSeason.id,
+    didStart: playerGameSummary.starter === true,
+    points: playerGameSummary.statistics.points,
+    secondsPlayed,
+    fieldGoals: playerGameSummary.statistics.field_goals_made,
+    fieldGoalAttempts: playerGameSummary.statistics.field_goals_att,
+    threePointers: playerGameSummary.statistics.three_points_made,
+    threePointAttempts: playerGameSummary.statistics.three_points_att,
+    twoPointers: playerGameSummary.statistics.two_points_made,
+    twoPointAttempts: playerGameSummary.statistics.two_points_att,
+    freeThrows: playerGameSummary.statistics.free_throws_made,
+    freeThrowAttempts: playerGameSummary.statistics.free_throws_att,
+    assists: playerGameSummary.statistics.assists,
+    rebounds: playerGameSummary.statistics.rebounds,
+    offRebounds: playerGameSummary.statistics.offensive_rebounds,
+    defRebounds: playerGameSummary.statistics.defensive_rebounds,
+    steals: playerGameSummary.statistics.steals,
+    blocks: playerGameSummary.statistics.blocks,
+    turnovers: playerGameSummary.statistics.turnovers,
+    personalFouls: playerGameSummary.statistics.personal_fouls,
+    plusMinus: playerGameSummary.statistics.pls_min,
+    offensivePossessions: (teamPossessionCount / teamSecondsPlayed) * secondsPlayed,
+    defensivePossessions: (opponentPossessionCount / teamSecondsPlayed) * secondsPlayed,
+  };
+
+  return { playerGameCreateData, playerSeason };
 }
 
-export async function prepareGameSummaryToPlayerGames({gameSummary, teamPossessionCount}: {gameSummary: GameSummary; teamPossessionCount: number}): Promise<Prisma.PlayerGameCreateArgs['data']> {
+export async function createPlayerGameAndUpdatePlayerSeason(
+  playerGameCreateData: Prisma.PlayerGameCreateArgs['data'],
+  playerSeason: PlayerSeason
+): Promise<void> {
+  await Promise.all([
+    createPlayerGame(playerGameCreateData),
+    updatePlayerSeason(playerSeason.id, {
+      gamesPlayed: playerSeason.gamesPlayed + 1,
+      gamesStarted: playerSeason.gamesStarted + (playerGameCreateData.didStart ? 1 : 0),
+      points: playerSeason.points + playerGameCreateData.points,
+      fieldGoals: playerSeason.fieldGoals + playerGameCreateData.fieldGoals,
+      fieldGoalAttempts: playerSeason.fieldGoalAttempts + playerGameCreateData.fieldGoalAttempts,
+      threePointers: playerSeason.threePointers + playerGameCreateData.threePointers,
+      threePointAttempts: playerSeason.threePointAttempts + playerGameCreateData.threePointAttempts,
+      twoPointers: playerSeason.twoPointers + playerGameCreateData.twoPointers,
+      twoPointAttempts: playerSeason.twoPointAttempts + playerGameCreateData.twoPointAttempts,
+      freeThrows: playerSeason.freeThrows + playerGameCreateData.freeThrows,
+      freeThrowAttempts: playerSeason.freeThrowAttempts + playerGameCreateData.freeThrowAttempts,
+      assists: playerSeason.assists + playerGameCreateData.assists,
+      rebounds: playerSeason.rebounds + playerGameCreateData.rebounds,
+      offRebounds: playerSeason.offRebounds + playerGameCreateData.offRebounds,
+      defRebounds: playerSeason.defRebounds + playerGameCreateData.defRebounds,
+      steals: playerSeason.steals + playerGameCreateData.steals,
+      blocks: playerSeason.blocks + playerGameCreateData.blocks,
+      turnovers: playerSeason.turnovers + playerGameCreateData.turnovers,
+      personalFouls: playerSeason.personalFouls + playerGameCreateData.personalFouls,
+      plusMinus: playerSeason.plusMinus + playerGameCreateData.plusMinus,
+      offensivePossessions:
+        Number(playerSeason.offensivePossessions) + Number(playerGameCreateData.offensivePossessions),
+      defensivePossessions:
+        Number(playerSeason.defensivePossessions) + Number(playerGameCreateData.defensivePossessions),
+    }),
+  ]);
+}
+
+export async function prepareGameSummaryCoachesDataToHeadCoachGame({
+  coachesData,
+  teamSeasonId,
+  gameId,
+}: { coachesData: CoachData[]; teamSeasonId: string; gameId: string }): Promise<
+  Prisma.HeadCoachGameCreateArgs['data']
+> {
+  const headCoachSummaryData = coachesData.filter((coach) => coach.position === 'Head Coach')[0];
+  const headCoachExternalId = headCoachSummaryData.id;
+
+  if (!headCoachExternalId) {
+    throw new Error('Unable to create HeadCoachGame: head coach external id missing');
+  }
+
+  let headCoach = await getCoachByExternalId(headCoachExternalId);
+
+  if (!headCoach) {
+    headCoach = await createCoach({ externalId: headCoachExternalId, fullName: headCoachSummaryData.full_name });
+  }
+
+  const teamGame = await getTeamGameByTeamSeasonIdAndGameId({ teamSeasonId, gameId });
+
   return {
-    playerId             String   
-    gameId               String   
-    playerSeasonId       String   
-    points               Int
-    secondsPlayed        Int      
-    fieldGoals           Int      
-    fieldGoalAttempts    Int      
-    threePointers        Int      
-    threePointAttempts   Int      
-    twoPointers          Int      
-    twoPointAttempts     Int      
-    freeThrows           Int      
-    freeThrowAttempts    Int      
-    assists              Int
-    rebounds             Int
-    offRebounds          Int      
-    defRebounds          Int      
-    steals               Int
-    blocks               Int
-    turnovers            Int
-    personalFouls        Int      
-    plusMinus            Int      
-    offensivePossessions Decimal  @default(0) @map("offensive_possessions")
-    defensivePossessions Decimal  @default(0) @map("defensive_possessions")
-    createdAt            DateTime @default(now()) @map("created_at")
-    updatedAt            DateTime @updatedAt @map("updated_at")
-}
-
-id                   String   @id @default(cuid())
-  playerId             String   @map("player_id")
-  teamSeasonId         String   @map("team_season_id")
-  seasonId             String   @map("season_id")
-  gamesPlayed          Int      @default(0) @map("games_played")
-  gamesStarted         Int      @default(0) @map("games_started")
-  isTwoWayContract     Boolean  @map("is_two_way_contract")
-  jerseyNumbers        Int[]    @map("jersey_number")
-  points               Int      @default(0)
-  secondsPlayed        Int      @default(0) @map("seconds_played")
-  fieldGoals           Int      @default(0) @map("field_goals")
-  fieldGoalAttempts    Int      @default(0) @map("field_goal_attempts")
-  threePointers        Int      @default(0) @map("three_pointers")
-  threePointAttempts   Int      @default(0) @map("three_point_attempts")
-  twoPointers          Int      @default(0) @map("two_pointers")
-  twoPointAttempts     Int      @default(0) @map("two_point_attempts")
-  freeThrows           Int      @default(0) @map("free_throws")
-  freeThrowAttempts    Int      @default(0) @map("free_throw_attempts")
-  assists              Int      @default(0)
-  rebounds             Int      @default(0)
-  offRebounds          Int      @default(0) @map("off_rebounds")
-  defRebounds          Int      @default(0) @map("def_rebounds")
-  steals               Int      @default(0)
-  blocks               Int      @default(0)
-  turnovers            Int      @default(0)
-  personalFouls        Int      @default(0) @map("personal_fouls")
-  plusMinus            Int      @default(0) @map("plus_minus")
-  offensivePossessions Decimal  @default(0) @map("offensive_possessions")
-  defensivePossessions Decimal  @default(0) @map("defensive_possessions")
-  createdAt            DateTime @default(now()) @map("created_at")
-  updatedAt            DateTime @updatedAt @map("updated_at")
-
-  player      Player       @relation(fields: [playerId], references: [id])
-  teamSeason  TeamSeason   @relation(fields: [teamSeasonId], references: [id])
-  season      Season       @relation(fields: [seasonId], references: [id])
-  playerGames PlayerGame[]
-
-  @@map("player_season")
+    coachId: headCoach.id,
+    teamGameId: teamGame.id,
+    teamSeasonId,
+  };
 }
 
 function convertGameMinutesToSeconds(minutes: string): number {
   return Number(minutes.split(':')[0]) * 60 + Number(minutes.split(':')[1]);
 }
-
-const playerGameCreate = 
