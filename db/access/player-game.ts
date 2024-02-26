70a87f4edd4d673c152c6f8e90dd9f135f71e582
Ryan Hammer <ryanhammer1@protonmail.com>
2024-02-25 19:50:30 -0500

    access function to create a player game

---
+++ db/access/player-game.ts
@@ -0,0 +1,8 @@
+import { PlayerGame, Prisma } from '@prisma/client';
+import { platformDB } from '../prisma';
+
+export async function createPlayerGame(playerGameData: Prisma.PlayerGameCreateArgs['data']): Promise<PlayerGame> {
+  return platformDB.playerGame.create({
+    data: playerGameData,
+  });
+}
