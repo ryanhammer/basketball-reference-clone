714d46a30cfa95f978657c2da52e745b5fd7d6fc
Ryan Hammer <ryanhammer1@protonmail.com>
2024-02-27 07:04:24 -0500

    add delay to handle api rate limits

--- app/utils/sportradar-access-functions.ts
+++ app/utils/sportradar-access-functions.ts
@@ -1,4 +1,5 @@
 import { PlayerProfile } from '../types/sportradar/player-profile';
+import { delay } from '../utils/sportradar-data-helpers';
 
 export async function getPlayerProfileBySportradarId(sportradarPlayerId: string): Promise<PlayerProfile> {
   const apiUrl = `http://api.sportradar.us/nba/trial/v8/en/players/${sportradarPlayerId}/profile.json?api_key=${process.env.SPORTRADAR_API_KEY}`;
@@ -12,6 +13,8 @@ export async function getPlayerProfileBySportradarId(sportradarPlayerId: string)
   }
 
   const data = await response.json();
+
+  await delay(1250);
 
   return data as PlayerProfile;
 }
